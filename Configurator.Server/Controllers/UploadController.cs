c34e2432a3825368ab1f9c499e2e32fb2473485c
mvkranenburg <53485326+mvkranenburg@users.noreply.github.com>
2023-06-17 14:12:57 +0200

    Initial upload controller method for ESI files

--- Configurator.Server/Controllers/UploadController.cs
+++ Configurator.Server/Controllers/UploadController.cs
@@ -1,83 +1,40 @@
 using System;
 using System.IO;
+using System.Xml.Linq;
+using System.Xml.XPath;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Hosting;
 
 namespace Configurator.Server.Controllers
 {
-    public partial class UploadController : Controller
+    public partial class UploadController : ControllerBase
     {
-        private readonly IWebHostEnvironment environment;
+        private readonly IWebHostEnvironment _environment;
+        private readonly ILogger _logger;
 
-        public UploadController(IWebHostEnvironment environment)
+        public UploadController(IWebHostEnvironment environment, ILogger<UploadController> logger)
         {
-            this.environment = environment;
+            _environment = environment;
+            _logger = logger;
         }
 
         // Single file upload
-        [HttpPost("upload/single")]
-        public IActionResult Single(IFormFile file)
+        [HttpPost("upload/esi")]
+        public IActionResult EtherCATSlaveInformation(IFormFile file)
         {
             try
             {
-                // Put your code here
-                return StatusCode(200);
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(500, ex.Message);
-            }
-        }
-
-        // Multiple files upload
-        [HttpPost("upload/multiple")]
-        public IActionResult Multiple(IFormFile[] files)
-        {
-            try
-            {
-                // Put your code here
-                return StatusCode(200);
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(500, ex.Message);
-            }
-        }
-
-        // Multiple files upload with parameter
-        [HttpPost("upload/{id}")]
-        public IActionResult Post(IFormFile[] files, int id)
-        {
-            try
-            {
-                // Put your code here
-                return StatusCode(200);
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(500, ex.Message);
-            }
-        }
-
-        // Image file upload (used by HtmlEditor components)
-        [HttpPost("upload/image")]
-        public IActionResult Image(IFormFile file)
-        {
-            try
-            {
-                var fileName = $"{Guid.NewGuid()}{Path.GetExtension(file.FileName)}";
-
-                using (var stream = new FileStream(Path.Combine(environment.WebRootPath, fileName), FileMode.Create))
+                // TODO: Validate IFormFile input against XSD
+                var esi = XDocument.Load(file.OpenReadStream());
+                var deviceTypes = esi.XPathSelectElements("//Descriptions/Devices/Device/Type").Select(e => e.Value);
+                foreach (var deviceType in deviceTypes)
                 {
-                    // Save the file
-                    file.CopyTo(stream);
-
-                    // Return the URL of the file
-                    var url = Url.Content($"~/{fileName}");
-
-                    return Ok(new { Url = url });
+                    _logger.LogDebug("Device: {deviceType}", deviceType);
                 }
+
+                // Put your code here
+                return StatusCode(200);
             }
             catch (Exception ex)
             {
--- Configurator.Server/Program.cs
+++ Configurator.Server/Program.cs
@@ -18,6 +18,8 @@ builder.Services.AddScoped<NotificationService>();
 builder.Services.AddScoped<TooltipService>();
 builder.Services.AddScoped<ContextMenuService>();
 
+builder.Logging.AddConsole();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
