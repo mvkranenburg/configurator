44bdb2d271805318cf182b683741c4039154ff03
mvkranenburg <53485326+mvkranenburg@users.noreply.github.com>
2023-07-06 22:52:59 +0200

    Moved basic XML stream parsing to EtherCATInfo class to the EtherCATInfoXmlSchema project

--- Configurator.Server/Controllers/UploadController.cs
+++ Configurator.Server/Controllers/UploadController.cs
@@ -1,7 +1,6 @@
-using System.Globalization;
-using System.Xml.Serialization;
 using Microsoft.AspNetCore.Mvc;
 using Configurator.Models;
+using static EtherCATInfoXmlSchema.Parser;
 
 namespace Configurator.Server.Controllers
 {
@@ -24,30 +23,6 @@ namespace Configurator.Server.Controllers
         /// <param name="logger">Logger instance.</param>
         public UploadController(IWebHostEnvironment environment, ILogger<UploadController> logger) =>
             (_environment, _logger) = (environment, logger);
-
-        /// <summary>
-        /// Parse xs:simpleType HexDecValue to long.
-        /// </summary>
-        /// <param name="hexDecValue">HexDecValue to parse.</param>
-        /// <returns>Parsed value.</returns>
-        private static long ParseHexDecValue(string hexDecValue)
-        {
-            if (hexDecValue.StartsWith("#x"))
-                return long.Parse(hexDecValue[2..], NumberStyles.HexNumber);
-            else
-                return long.Parse(hexDecValue);
-        }
-
-        /// <summary>
-        /// Parse xs:complexType NameType to string.
-        /// </summary>
-        /// <param name="nameTypes">Collection of NameType to parse.</param>
-        /// <param name="lcid">Language ID to parse, default is 1033 (English)</param>
-        /// <returns>Parsed value.</returns>
-        private static string ParseNameType(IEnumerable<EtherCATInfoXmlSchema.NameType> nameTypes, int lcId = 1033)
-        {
-            return nameTypes.Where(n => n.LcId == lcId).FirstOrDefault()?.Value;
-        }
 
         /// <summary>
         /// Parse xs:complexType AccessType to EtherCATObjectAccess.
@@ -108,86 +83,6 @@ namespace Configurator.Server.Controllers
         }
 
         /// <summary>
-        /// Deserialize and pre-process EtherCAT Slave Information (ESI) file. The deserialized ESI is pre-processed before return
-        /// for easier parsing. The pre-processing steps are:
-        /// - Expand SubItems referring to an ArrayInfo DataType
-        ///
-        /// Constraints:
-        /// - Only Devices with a single Profile are supported, exception is thrown otherwise
-        /// - Only DataTypes with a single ArrayInfo are supported, exception is thrown otherwise
-        /// </summary>
-        /// <param name="stream">Stream containing the ESI file.</param>
-        /// <returns><c>EtherCATInfoXmlSchema.EtherCATInfo</c> representing the ESI file.</returns>
-        /// <exception cref="System.InvalidOperationException">Thrown when XML parsing fails.</exception>
-        private static EtherCATInfoXmlSchema.EtherCATInfo DeserializeEtherCATInfo(System.IO.Stream stream)
-        {
-            // TODO: Validate input against XSD
-
-            // Deserialize XML
-            var serializer = new XmlSerializer(typeof(EtherCATInfoXmlSchema.EtherCATInfo));
-            var esi = (EtherCATInfoXmlSchema.EtherCATInfo)serializer.Deserialize(stream);
-
-            // Expand SubItems referring to an ArrayInfo DataType
-            for (var i = 0; i < esi.Descriptions.Devices.Count; i++)
-            {
-                // Only continue if a Profile is defined
-                var profile = esi.Descriptions.Devices[i].Profile;
-                if (profile.Count == 0)
-                    continue;
-
-                var dataTypes = profile.Single().Dictionary.DataTypes;
-                var arrayInfos = dataTypes.Where(d => d.ArrayInfo.Count() > 0)
-                    .Select(d => (d.Name, d.BaseType, d.BitSize, d.ArrayInfo.Single().LBound, d.ArrayInfo.Single().Elements))
-                    .ToDictionary(d => d.Name);
-
-                for (var j = 0; j < dataTypes.Count; j++)
-                {
-                    var subItems = dataTypes[j].SubItem;
-
-                    // CoE data type ARRAY conditions (ETG.2000 v1.0.14 table 10):
-                    // - must contain exactly two SubItems
-                    // - 1st element must have SubIdx 0
-                    // - 1st element must have Type USINT
-                    // - 2nd element must have no SubIdx
-                    // - 2nd element must have ArrayInfo datatype
-                    if
-                    (
-                        subItems.Count == 2 &&
-                        ParseHexDecValue(subItems[0].SubIdx) == 0 &&
-                        subItems[0].Type == "USINT" &&
-                        subItems[1].SubIdx == null &&
-                        arrayInfos.ContainsKey(subItems[1].Type)
-                    )
-                    {
-                        var bitOffs = subItems[1].BitOffs;
-                        var flags = subItems[1].Flags;
-                        var arrayInfo = arrayInfos[subItems[1].Type];
-
-                        subItems.RemoveAt(1);
-
-                        for (var k = arrayInfo.LBound; k < arrayInfo.LBound + arrayInfo.Elements; k++)
-                        {
-                            var subItem = new EtherCATInfoXmlSchema.SubItemType
-                            {
-                                SubIdx = $"{k}",
-                                Name = $"Element[{k - 1}]",
-                                Type = arrayInfo.BaseType,
-                                BitSize = arrayInfo.BitSize,
-                                BitOffs = bitOffs,
-                                Flags = flags,
-                            };
-                            subItems.Add(subItem);
-
-                            bitOffs += arrayInfo.BitSize;
-                        }
-                    }
-                }
-            }
-
-            return esi;
-        }
-
-        /// <summary>
         /// Upload handler for EtherCAT Slave Information (ESI) file on the path <c>upload/esi</c>. The 
         /// ESI file is converted to an <c>IEnumerable&lt;EtherCATDevice&gt;</c> collection of devices
         /// and returned to the caller on success.
@@ -200,7 +95,7 @@ namespace Configurator.Server.Controllers
             try
             {
                 // Deserialize an pre-process ESI
-                var esi = DeserializeEtherCATInfo(file.OpenReadStream());
+                var esi = Parse(file.OpenReadStream());
 
                 // Parse devices
                 var devices = esi.Descriptions.Devices.Select(d => new EtherCATDevice
@@ -296,7 +191,7 @@ namespace Configurator.Server.Controllers
             {
                 return BadRequest($"XML content error: {ex.Message}");
             }
-            catch (InvalidOperationException ex)
+            catch (System.Xml.XmlException ex)
             {
                 return BadRequest($"XML syntax error: {ex.Message}");
             }
---
+++ EtherCATInfoXmlSchema/Parser.cs
@@ -0,0 +1,121 @@
+namespace EtherCATInfoXmlSchema;
+
+using System.Globalization;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+
+public static class Parser
+{
+    /// <summary>
+    /// Parse xs:simpleType HexDecValue to long.
+    /// </summary>
+    /// <param name="hexDecValue">HexDecValue to parse.</param>
+    /// <returns>Parsed value.</returns>
+    public static long ParseHexDecValue(string hexDecValue)
+    {
+        if (hexDecValue.StartsWith("#x"))
+            return long.Parse(hexDecValue[2..], NumberStyles.HexNumber);
+        else
+            return long.Parse(hexDecValue);
+    }
+
+    /// <summary>
+    /// Parse xs:complexType NameType to string.
+    /// </summary>
+    /// <param name="nameTypes">Collection of NameType to parse.</param>
+    /// <param name="lcid">Language ID to parse, default is 1033 (English)</param>
+    /// <returns>Parsed value.</returns>
+    public static string? ParseNameType(IEnumerable<NameType> nameTypes, int lcId = 1033)
+    {
+        return nameTypes.Where(n => n.LcId == lcId).SingleOrDefault()?.Value;
+    }
+
+    private static void ExpandDataTypeSubItems(ref EtherCATInfo? esi)
+    {
+        for (var i = 0; i < esi?.Descriptions.Devices.Count; i++)
+        {
+            // Only continue if a Profile is defined
+            var profile = esi.Descriptions.Devices[i].Profile;
+            if (profile.Count == 0)
+                continue;
+
+            var dataTypes = profile.Single().Dictionary.DataTypes;
+            var arrayInfos = dataTypes.Where(d => d.ArrayInfo.Count > 0)
+                .Select(d => (d.Name, d.BaseType, d.BitSize, d.ArrayInfo.Single().LBound, d.ArrayInfo.Single().Elements))
+                .ToDictionary(d => d.Name);
+
+            for (var j = 0; j < dataTypes.Count; j++)
+            {
+                var subItems = dataTypes[j].SubItem;
+
+                // CoE data type ARRAY conditions (ETG.2000 v1.0.14 table 10):
+                // - must contain exactly two SubItems
+                // - 1st element must have SubIdx 0
+                // - 1st element must have Type USINT
+                // - 2nd element must have no SubIdx
+                // - 2nd element must have ArrayInfo datatype
+                if
+                (
+                    subItems.Count == 2 &&
+                    ParseHexDecValue(subItems[0].SubIdx) == 0 &&
+                    subItems[0].Type == "USINT" &&
+                    subItems[1].SubIdx == null &&
+                    arrayInfos.ContainsKey(subItems[1].Type)
+                )
+                {
+                    var bitOffs = subItems[1].BitOffs;
+                    var flags = subItems[1].Flags;
+                    var (Name, BaseType, BitSize, LBound, Elements) = arrayInfos[subItems[1].Type];
+
+                    subItems.RemoveAt(1);
+
+                    for (var k = LBound; k < LBound + Elements; k++)
+                    {
+                        var subItem = new SubItemType
+                        {
+                            SubIdx = $"{k}",
+                            Name = $"Element[{k - 1}]",
+                            Type = BaseType,
+                            BitSize = BitSize,
+                            BitOffs = bitOffs,
+                            Flags = flags,
+                        };
+                        subItems.Add(subItem);
+
+                        bitOffs += BitSize;
+                    }
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// Deserialize and pre-process EtherCAT Slave Information (ESI) file. The deserialized ESI is pre-processed before return
+    /// for easier parsing. The pre-processing steps are:
+    /// - Expand SubItems referring to an ArrayInfo DataType
+    ///
+    /// Constraints:
+    /// - Only Devices with a single Profile are supported, exception is thrown otherwise
+    /// - Only DataTypes with a single ArrayInfo are supported, exception is thrown otherwise
+    /// </summary>
+    /// <param name="stream">Stream containing the ESI file.</param>
+    /// <returns><c>EtherCATInfo?</c> representing the ESI file.</returns>
+    /// <exception cref="XmlException">Thrown when XML parsing fails.</exception>
+    public static EtherCATInfo? Parse(Stream stream)
+    {
+        // Use System.Xml.Linq for schema validation and better error reporting
+        _ = XDocument.Load(stream);
+
+        // Use System.Xml.Serialization.XmlSerializer to parse the content
+        var serializer = new XmlSerializer(typeof(EtherCATInfo));
+
+        stream.Seek(0, SeekOrigin.Begin);
+        var esi = (EtherCATInfo?)serializer.Deserialize(stream);
+
+        // Expand SubItems referring to an ArrayInfo DataType
+        ExpandDataTypeSubItems(ref esi);
+
+        return esi;
+    }
+
+}
